d375a92e725727a14ea654feee974de73bc4a738
yongjoo <yongjoo@wannabit.io>
2022-01-12 15:41:21 +0900

    [IOSWL-86] remove lcd post for staking tx

--- Cosmostation/Controller/GenTx/ReInvestCheckViewController.swift
+++ Cosmostation/Controller/GenTx/ReInvestCheckViewController.swift
@@ -42,32 +42,16 @@ class ReInvestCheckViewController: BaseViewController, PasswordViewDelegate {
     
     func onUpdateView() {
         mDpDecimal = WUtils.mainDivideDecimal(chainType)
+        rewardLabel.attributedText = WUtils.displayAmount2(pageHolderVC.mReinvestReward!.amount, rewardLabel.font, mDpDecimal, mDpDecimal)
+        feeLabel.attributedText = WUtils.displayAmount2((pageHolderVC.mFee?.amount[0].amount)!, feeLabel.font, mDpDecimal, mDpDecimal)
         
-        if (WUtils.isGRPC(chainType)) {
-            rewardLabel.attributedText = WUtils.displayAmount2(pageHolderVC.mReinvestReward!.amount, rewardLabel.font, mDpDecimal, mDpDecimal)
-            feeLabel.attributedText = WUtils.displayAmount2((pageHolderVC.mFee?.amount[0].amount)!, feeLabel.font, mDpDecimal, mDpDecimal)
-            
-            let currentDelegation = BaseData.instance.getDelegated_gRPC(pageHolderVC.mTargetValidator_gRPC?.operatorAddress)
-            let expectedDelegation = currentDelegation.adding(NSDecimalNumber.init(string: pageHolderVC.mReinvestReward!.amount))
-            currentDelegateAmount.attributedText = WUtils.displayAmount2(currentDelegation.stringValue, currentDelegateAmount.font, mDpDecimal, mDpDecimal)
-            expectedDelegateAmount.attributedText = WUtils.displayAmount2(expectedDelegation.stringValue, expectedDelegateAmount.font, mDpDecimal, mDpDecimal)
-            
-            validatorLabel.text = pageHolderVC.mTargetValidator_gRPC?.description_p.moniker
-            memoLabel.text = pageHolderVC.mMemo
-            
-        } else {
-            rewardLabel.attributedText = WUtils.displayAmount2(pageHolderVC.mReinvestReward!.amount, rewardLabel.font, mDpDecimal, mDpDecimal)
-            feeLabel.attributedText = WUtils.displayAmount2((pageHolderVC.mFee?.amount[0].amount)!, feeLabel.font, mDpDecimal, mDpDecimal)
-            
-            let currentDelegation = BaseData.instance.delegatedAmountByValidator(self.pageHolderVC.mTargetValidator!.operator_address)
-            let expected = currentDelegation.adding(NSDecimalNumber.init(string: pageHolderVC.mReinvestReward!.amount))
-            currentDelegateAmount.attributedText = WUtils.displayAmount2(currentDelegation.stringValue, currentDelegateAmount.font, mDpDecimal, mDpDecimal)
-            expectedDelegateAmount.attributedText = WUtils.displayAmount2(expected.stringValue, expectedDelegateAmount.font, mDpDecimal, mDpDecimal)
-            
-            validatorLabel.text = pageHolderVC.mTargetValidator?.description.moniker
-            memoLabel.text = pageHolderVC.mMemo
-            
-        }
+        let currentDelegation = BaseData.instance.getDelegated_gRPC(pageHolderVC.mTargetValidator_gRPC?.operatorAddress)
+        let expectedDelegation = currentDelegation.adding(NSDecimalNumber.init(string: pageHolderVC.mReinvestReward!.amount))
+        currentDelegateAmount.attributedText = WUtils.displayAmount2(currentDelegation.stringValue, currentDelegateAmount.font, mDpDecimal, mDpDecimal)
+        expectedDelegateAmount.attributedText = WUtils.displayAmount2(expectedDelegation.stringValue, expectedDelegateAmount.font, mDpDecimal, mDpDecimal)
+        
+        validatorLabel.text = pageHolderVC.mTargetValidator_gRPC?.description_p.moniker
+        memoLabel.text = pageHolderVC.mMemo
     }
     
     override func enableUserInteraction() {
@@ -113,122 +97,9 @@ class ReInvestCheckViewController: BaseViewController, PasswordViewDelegate {
     
     func passwordResponse(result: Int) {
         if (result == PASSWORD_RESUKT_OK) {
-            if (WUtils.isGRPC(chainType)) {
-                self.onFetchgRPCAuth(pageHolderVC.mAccount!)
-            } else {
-                self.onFetchAccountInfo(pageHolderVC.mAccount!)
-            }
+            self.onFetchgRPCAuth(pageHolderVC.mAccount!)
         }
     }
-    
-    func onFetchAccountInfo(_ account: Account) {
-        self.showWaittingAlert()
-        let request = Alamofire.request(BaseNetWork.accountInfoUrl(chainType, account.account_address), method: .get, parameters: [:], encoding: URLEncoding.default, headers: [:])
-        request.responseJSON { (response) in
-            switch response.result {
-            case .success(let res):
-                if (self.chainType == ChainType.KAVA_MAIN || self.chainType == ChainType.KAVA_TEST) {
-                    guard let info = res as? [String : Any] else {
-                        _ = BaseData.instance.deleteBalance(account: account)
-                        self.hideWaittingAlert()
-                        self.onShowToast(NSLocalizedString("error_network", comment: ""))
-                        return
-                    }
-                    let accountInfo = KavaAccountInfo.init(info)
-                    _ = BaseData.instance.updateAccount(WUtils.getAccountWithKavaAccountInfo(account, accountInfo))
-                    BaseData.instance.updateBalances(account.account_id, WUtils.getBalancesWithKavaAccountInfo(account, accountInfo))
-                    self.onGenReinvest()
-                    
-                } else {
-                    guard let responseData = res as? NSDictionary,
-                        let info = responseData.object(forKey: "result") as? [String : Any] else {
-                            _ = BaseData.instance.deleteBalance(account: account)
-                            self.hideWaittingAlert()
-                            self.onShowToast(NSLocalizedString("error_network", comment: ""))
-                            return
-                    }
-                    let accountInfo = AccountInfo.init(info)
-                    _ = BaseData.instance.updateAccount(WUtils.getAccountWithAccountInfo(account, accountInfo))
-                    BaseData.instance.updateBalances(account.account_id, WUtils.getBalancesWithAccountInfo(account, accountInfo))
-                    self.onGenReinvest()
-                    
-                }
-                
-            case .failure( _):
-                self.hideWaittingAlert()
-                self.onShowToast(NSLocalizedString("error_network", comment: ""))
-            }
-        }
-    }
-    
-    func onGenReinvest() {
-        DispatchQueue.global().async {
-            var msgList = Array<Msg>()
-            let rewardMsg = MsgGenerator.genGetRewardMsg(self.pageHolderVC.mAccount!.account_address,
-                                                         self.pageHolderVC.mTargetValidator!.operator_address,
-                                                         self.chainType!)
-            
-            let delegatemsg = MsgGenerator.genDelegateMsg(self.pageHolderVC.mAccount!.account_address,
-                                                          self.pageHolderVC.mTargetValidator!.operator_address,
-                                                          self.pageHolderVC.mReinvestReward!,
-                                                          self.chainType!)
-            msgList.append(rewardMsg)
-            msgList.append(delegatemsg)
-            
-            let stdMsg = MsgGenerator.getToSignMsg(BaseData.instance.getChainId(self.chainType),
-                                                   String(self.pageHolderVC.mAccount!.account_account_numner),
-                                                   String(self.pageHolderVC.mAccount!.account_sequence_number),
-                                                   msgList,
-                                                   self.pageHolderVC.mFee!,
-                                                   self.pageHolderVC.mMemo!)
-            
-            let stdTx = KeyFac.getStdTx(self.pageHolderVC.privateKey!, self.pageHolderVC.publicKey!,
-                                        msgList, stdMsg,
-                                        self.pageHolderVC.mAccount!, self.pageHolderVC.mFee!, self.pageHolderVC.mMemo!)
-            
-            DispatchQueue.main.async(execute: {
-                let postTx = PostTx.init("sync", stdTx.value)
-                let encoder = JSONEncoder()
-                encoder.outputFormatting = .sortedKeys
-                let data = try? encoder.encode(postTx)
-                
-                do {
-                    let params = try JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? [String: Any]
-//                    print("params ", params)
-                    let request = Alamofire.request(BaseNetWork.broadcastUrl(self.chainType), method: .post, parameters: params, encoding: JSONEncoding.default, headers: [:])
-                    request.validate()
-                    request.responseJSON { response in
-                        var txResult = [String:Any]()
-                        switch response.result {
-                        case .success(let res):
-                            print("ReInvest ", res)
-                            if let result = res as? [String : Any]  {
-                                txResult = result
-                            }
-                        case .failure(let error):
-                            print("ReInvest error ", error)
-                            if (response.response?.statusCode == 500) {
-                                txResult["net_error"] = 500
-                            }
-                        }
-                        
-                        if (self.waitAlert != nil) {
-                            self.waitAlert?.dismiss(animated: true, completion: {
-                                self.onStartTxDetail(txResult)
-                            })
-                        }
-                    }
-                    
-                }catch {
-                    print(error)
-                }
-                
-            });
-        }
-    }
-    
-    
-    
     
     func onFetchgRPCAuth(_ account: Account) {
         self.showWaittingAlert()
--- Cosmostation/Controller/GenTx/StepRewardCheckViewController.swift
+++ Cosmostation/Controller/GenTx/StepRewardCheckViewController.swift
@@ -79,239 +79,71 @@ class StepRewardCheckViewController: BaseViewController, PasswordViewDelegate{
     }
 
     func checkIsWasteFee() -> Bool {
-        if (WUtils.isGRPC(chainType)) {
-            var selectedRewardSum = NSDecimalNumber.zero
-            for validator in pageHolderVC.mRewardTargetValidators_gRPC {
-                let amount = BaseData.instance.getReward_gRPC(WUtils.getMainDenom(chainType), validator.operatorAddress)
-                selectedRewardSum = selectedRewardSum.adding(amount)
-            }
-            if (NSDecimalNumber.init(string: pageHolderVC.mFee?.amount[0].amount).compare(selectedRewardSum).rawValue > 0 ) {
-                return true
-            }
-
-        } else {
-            var selectedRewardSum = NSDecimalNumber.zero
-            pageHolderVC.mRewards.forEach { coin in
-                if (coin.denom == WUtils.getMainDenom(chainType)) {
-                    selectedRewardSum = selectedRewardSum.adding(WUtils.plainStringToDecimal(coin.amount))
-                }
-            }
-            if (NSDecimalNumber.init(string: pageHolderVC.mFee!.amount[0].amount).compare(selectedRewardSum).rawValue > 0 ) {
-                return true
-            }
+        var selectedRewardSum = NSDecimalNumber.zero
+        for validator in pageHolderVC.mRewardTargetValidators_gRPC {
+            let amount = BaseData.instance.getReward_gRPC(WUtils.getMainDenom(chainType), validator.operatorAddress)
+            selectedRewardSum = selectedRewardSum.adding(amount)
+        }
+        if (NSDecimalNumber.init(string: pageHolderVC.mFee?.amount[0].amount).compare(selectedRewardSum).rawValue > 0 ) {
+            return true
         }
         return false
     }
     
     func onUpdateView() {
         mDpDecimal = WUtils.mainDivideDecimal(chainType)
-        
-        if (WUtils.isGRPC(chainType)) {
-            var monikers = ""
-            for validator in pageHolderVC.mRewardTargetValidators_gRPC {
-                if(monikers.count > 0) {
-                    monikers = monikers + ",   " + validator.description_p.moniker
-                } else {
-                    monikers = validator.description_p.moniker
-                }
-            }
-            fromValidatorLabel.text = monikers
-            memoLabel.text = pageHolderVC.mMemo
-            recipientLabel.text = pageHolderVC.mRewardAddress
-            recipientLabel.adjustsFontSizeToFitWidth = true
-            
-            var selectedRewardSum = NSDecimalNumber.zero
-            for validator in pageHolderVC.mRewardTargetValidators_gRPC {
-                let amount = BaseData.instance.getReward_gRPC(WUtils.getMainDenom(chainType), validator.operatorAddress)
-                selectedRewardSum = selectedRewardSum.adding(amount)
-            }
-            
-            rewardAmoutLaebl.attributedText = WUtils.displayAmount2(selectedRewardSum.stringValue, rewardAmoutLaebl.font, mDpDecimal, mDpDecimal)
-            feeAmountLabel.attributedText = WUtils.displayAmount2(pageHolderVC.mFee?.amount[0].amount, feeAmountLabel.font, mDpDecimal, mDpDecimal)
-            
-            let userBalance: NSDecimalNumber = BaseData.instance.getAvailableAmount_gRPC(WUtils.getMainDenom(chainType))
-            let expectedAmount = userBalance.adding(selectedRewardSum).subtracting(WUtils.plainStringToDecimal(pageHolderVC.mFee?.amount[0].amount))
-            expectedAmountLabel.attributedText = WUtils.displayAmount2(expectedAmount.stringValue, rewardAmoutLaebl.font, mDpDecimal, mDpDecimal)
-            
-            if (pageHolderVC.mAccount?.account_address == pageHolderVC.mRewardAddress) {
-                recipientTitleLabel.isHidden = true
-                recipientLabel.isHidden = true
-                
-                expectedSeparator.isHidden = false
-                expectedAmountTitle.isHidden = false
-                expectedAmountLabel.isHidden = false
-                expectedDenomLabel.isHidden = false
-                
+        var monikers = ""
+        for validator in pageHolderVC.mRewardTargetValidators_gRPC {
+            if(monikers.count > 0) {
+                monikers = monikers + ",   " + validator.description_p.moniker
             } else {
-                recipientTitleLabel.isHidden = false
-                recipientLabel.isHidden = false
-                
-                expectedSeparator.isHidden = true
-                expectedAmountTitle.isHidden = true
-                expectedAmountLabel.isHidden = true
-                expectedDenomLabel.isHidden = true
+                monikers = validator.description_p.moniker
             }
+        }
+        fromValidatorLabel.text = monikers
+        memoLabel.text = pageHolderVC.mMemo
+        recipientLabel.text = pageHolderVC.mRewardAddress
+        recipientLabel.adjustsFontSizeToFitWidth = true
+        
+        var selectedRewardSum = NSDecimalNumber.zero
+        for validator in pageHolderVC.mRewardTargetValidators_gRPC {
+            let amount = BaseData.instance.getReward_gRPC(WUtils.getMainDenom(chainType), validator.operatorAddress)
+            selectedRewardSum = selectedRewardSum.adding(amount)
+        }
+        
+        rewardAmoutLaebl.attributedText = WUtils.displayAmount2(selectedRewardSum.stringValue, rewardAmoutLaebl.font, mDpDecimal, mDpDecimal)
+        feeAmountLabel.attributedText = WUtils.displayAmount2(pageHolderVC.mFee?.amount[0].amount, feeAmountLabel.font, mDpDecimal, mDpDecimal)
+        
+        let userBalance: NSDecimalNumber = BaseData.instance.getAvailableAmount_gRPC(WUtils.getMainDenom(chainType))
+        let expectedAmount = userBalance.adding(selectedRewardSum).subtracting(WUtils.plainStringToDecimal(pageHolderVC.mFee?.amount[0].amount))
+        expectedAmountLabel.attributedText = WUtils.displayAmount2(expectedAmount.stringValue, rewardAmoutLaebl.font, mDpDecimal, mDpDecimal)
+        
+        if (pageHolderVC.mAccount?.account_address == pageHolderVC.mRewardAddress) {
+            recipientTitleLabel.isHidden = true
+            recipientLabel.isHidden = true
+            
+            expectedSeparator.isHidden = false
+            expectedAmountTitle.isHidden = false
+            expectedAmountLabel.isHidden = false
+            expectedDenomLabel.isHidden = false
             
         } else {
-            var selectedRewardSum = NSDecimalNumber.zero
-            pageHolderVC.mRewards.forEach { coin in
-                if (coin.denom == WUtils.getMainDenom(chainType)) {
-                    selectedRewardSum = selectedRewardSum.adding(WUtils.plainStringToDecimal(coin.amount))
-                }
-            }
-            rewardAmoutLaebl.attributedText = WUtils.displayAmount2(selectedRewardSum.stringValue, rewardAmoutLaebl.font, mDpDecimal, mDpDecimal)
-            feeAmountLabel.attributedText = WUtils.displayAmount2((pageHolderVC.mFee?.amount[0].amount)!, feeAmountLabel.font, mDpDecimal, mDpDecimal)
+            recipientTitleLabel.isHidden = false
+            recipientLabel.isHidden = false
             
-            var userBalance = NSDecimalNumber.zero
-            for balance in pageHolderVC.mBalances {
-                if (balance.balance_denom == WUtils.getMainDenom(chainType)) {
-                    userBalance = userBalance.adding(WUtils.localeStringToDecimal(balance.balance_amount))
-                }
-            }
-            
-            let expectedAmount = userBalance.adding(selectedRewardSum).subtracting(WUtils.localeStringToDecimal((pageHolderVC.mFee?.amount[0].amount)!))
-            expectedAmountLabel.attributedText = WUtils.displayAmount2(expectedAmount.stringValue, rewardAmoutLaebl.font, mDpDecimal, mDpDecimal)
-            
-            var monikers = ""
-            for validator in pageHolderVC.mRewardTargetValidators {
-                if(monikers.count > 0) {
-                    monikers = monikers + ",   " + validator.description.moniker
-                } else {
-                    monikers = validator.description.moniker
-                }
-            }
-            fromValidatorLabel.text = monikers
-            memoLabel.text = pageHolderVC.mMemo
-            
-            recipientLabel.text = pageHolderVC.mRewardAddress
-            recipientLabel.adjustsFontSizeToFitWidth = true
-            
-            if (pageHolderVC.mAccount?.account_address == pageHolderVC.mRewardAddress) {
-                recipientTitleLabel.isHidden = true
-                recipientLabel.isHidden = true
-        
-                expectedSeparator.isHidden = false
-                expectedAmountTitle.isHidden = false
-                expectedAmountLabel.isHidden = false
-                expectedDenomLabel.isHidden = false
-                
-            } else {
-                recipientTitleLabel.isHidden = false
-                recipientLabel.isHidden = false
-                
-                expectedSeparator.isHidden = true
-                expectedAmountTitle.isHidden = true
-                expectedAmountLabel.isHidden = true
-                expectedDenomLabel.isHidden = true
-            }
-            
+            expectedSeparator.isHidden = true
+            expectedAmountTitle.isHidden = true
+            expectedAmountLabel.isHidden = true
+            expectedDenomLabel.isHidden = true
         }
         
     }
     
     func passwordResponse(result: Int) {
         if (result == PASSWORD_RESUKT_OK) {
-            if (WUtils.isGRPC(chainType)) {
-                self.onFetchgRPCAuth(pageHolderVC.mAccount!)
-            } else {
-                self.onFetchAccountInfo(pageHolderVC.mAccount!)
-            }
+            self.onFetchgRPCAuth(pageHolderVC.mAccount!)
         }
     }
-    
-    func onFetchAccountInfo(_ account: Account) {
-        self.showWaittingAlert()
-        let request = Alamofire.request(BaseNetWork.accountInfoUrl(chainType, account.account_address), method: .get, parameters: [:], encoding: URLEncoding.default, headers: [:])
-        request.responseJSON { (response) in
-            switch response.result {
-            case .success(let res):
-                if (self.chainType == ChainType.KAVA_MAIN || self.chainType == ChainType.KAVA_TEST) {
-                    guard let info = res as? [String : Any] else {
-                        _ = BaseData.instance.deleteBalance(account: account)
-                        self.hideWaittingAlert()
-                        self.onShowToast(NSLocalizedString("error_network", comment: ""))
-                        return
-                    }
-                    let accountInfo = KavaAccountInfo.init(info)
-                    _ = BaseData.instance.updateAccount(WUtils.getAccountWithKavaAccountInfo(account, accountInfo))
-                    BaseData.instance.updateBalances(account.account_id, WUtils.getBalancesWithKavaAccountInfo(account, accountInfo))
-                    self.onGenGetRewardTx()
-                    
-                } else {
-                    guard let responseData = res as? NSDictionary,
-                        let info = responseData.object(forKey: "result") as? [String : Any] else {
-                            _ = BaseData.instance.deleteBalance(account: account)
-                            self.hideWaittingAlert()
-                            self.onShowToast(NSLocalizedString("error_network", comment: ""))
-                            return
-                    }
-                    let accountInfo = AccountInfo.init(info)
-                    _ = BaseData.instance.updateAccount(WUtils.getAccountWithAccountInfo(account, accountInfo))
-                    BaseData.instance.updateBalances(account.account_id, WUtils.getBalancesWithAccountInfo(account, accountInfo))
-                    self.onGenGetRewardTx()
-                }
-                
-            case .failure( _):
-                self.hideWaittingAlert()
-                self.onShowToast(NSLocalizedString("error_network", comment: ""))
-            }
-        }
-    }
-    
-    func onGenGetRewardTx() {
-        DispatchQueue.global().async {
-            var msgList = Array<Msg>()
-            for val in self.pageHolderVC.mRewardTargetValidators {
-                let msg = MsgGenerator.genGetRewardMsg(self.pageHolderVC.mAccount!.account_address, val.operator_address, self.chainType!)
-                msgList.append(msg)
-            }
-            let stdMsg = MsgGenerator.getToSignMsg(BaseData.instance.getChainId(self.chainType),
-                                                   String(self.pageHolderVC.mAccount!.account_account_numner),
-                                                   String(self.pageHolderVC.mAccount!.account_sequence_number),
-                                                   msgList,
-                                                   self.pageHolderVC.mFee!,
-                                                   self.pageHolderVC.mMemo!)
-            
-            let stdTx = KeyFac.getStdTx(self.pageHolderVC.privateKey!, self.pageHolderVC.publicKey!,
-                                        msgList, stdMsg,
-                                        self.pageHolderVC.mAccount!, self.pageHolderVC.mFee!, self.pageHolderVC.mMemo!)
-            
-            DispatchQueue.main.async(execute: {
-                let postTx = PostTx.init("sync", stdTx.value)
-                let encoder = JSONEncoder()
-                encoder.outputFormatting = .sortedKeys
-                let data = try? encoder.encode(postTx)
-                do {
-                    let params = try JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? [String: Any]
-                    let request = Alamofire.request(BaseNetWork.broadcastUrl(self.chainType), method: .post, parameters: params, encoding: JSONEncoding.default, headers: [:])
-                    request.responseJSON { response in
-                        var txResult = [String:Any]()
-                        switch response.result {
-                        case .success(let res):
-                            print("getReward ", res)
-                            if let result = res as? [String : Any]  {
-                                txResult = result
-                            }
-                        case .failure(let error):
-                            print("getRewarderror ", error)
-                            if (response.response?.statusCode == 500) {
-                                txResult["net_error"] = 500
-                            }
-                        }
-                        
-                        if (self.waitAlert != nil) {
-                            self.waitAlert?.dismiss(animated: true, completion: {
-                                self.onStartTxDetail(txResult)
-                            })
-                        }
-                    }
-                } catch {
-                    print(error)
-                }
-            });
-        }
-    }
-    
     
     func onFetchgRPCAuth(_ account: Account) {
         self.showWaittingAlert()
