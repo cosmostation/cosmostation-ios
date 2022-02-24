7dbe7ef6db99e40cb4875fc946819ebb99a03122
Kwonhyukjoon <hyukjoon@stamper.network>
2022-02-25 04:09:10 +0900

    [IOSWL-90] Update Evmos unbonding time to 7

--- Cosmostation/Controller/GenTx/StepUndelegateCheckViewController.swift
+++ Cosmostation/Controller/GenTx/StepUndelegateCheckViewController.swift
@@ -68,7 +68,7 @@ class StepUndelegateCheckViewController: BaseViewController, PasswordViewDelegat
             expectedDateLabel.text = WUtils.unbondingDateFromNow(28) + " (28days after)"
         } else if (chainType == ChainType.OSMOSIS_MAIN || chainType == ChainType.BITCANA_MAIN || chainType == ChainType.DESMOS_MAIN || chainType == ChainType.STARGAZE_MAIN || chainType == ChainType.UMEE_MAIN) {
             expectedDateLabel.text = WUtils.unbondingDateFromNow(14) + " (14days after)"
-        } else if (chainType == ChainType.AXELAR_MAIN ) {
+        } else if (chainType == ChainType.AXELAR_MAIN || chainType == ChainType.EVMOS_MAIN) {
             expectedDateLabel.text = WUtils.unbondingDateFromNow(7) + " (7days after)"
         } else {
             expectedDateLabel.text = WUtils.unbondingDateFromNow(21) + " (21days after)"
