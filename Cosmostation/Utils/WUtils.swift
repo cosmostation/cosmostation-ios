95d46d4d15a45a5ffb62f4b84dd72e964542770e
yongjoo <yongjoo@wannabit.io>
2022-04-13 17:27:31 +0900

    [IOSWL-138]Set gas zero for Crescent

--- Cosmostation/Utils/WUtils.swift
+++ Cosmostation/Utils/WUtils.swift
@@ -3100,7 +3100,6 @@ public class WUtils {
             chain == ChainType.LUM_MAIN || chain == ChainType.AXELAR_MAIN || chain == ChainType.KONSTELLATION_MAIN ||
             chain == ChainType.UMEE_MAIN || chain == ChainType.PROVENANCE_MAIN || chain == ChainType.EVMOS_MAIN ||
             chain == ChainType.CUDOS_MAIN || chain == ChainType.CERBERUS_MAIN || chain == ChainType.OMNIFLIX_MAIN ||
-            chain == ChainType.CRESCENT_MAIN ||
             chain == ChainType.COSMOS_TEST || chain == ChainType.IRIS_TEST || chain == ChainType.ALTHEA_TEST ||
             chain == ChainType.CRESCENT_TEST) {
             if (type == COSMOS_MSG_TYPE_TRANSFER2) {
