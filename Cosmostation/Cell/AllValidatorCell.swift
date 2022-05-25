8e4c774b0a5b0e51beb974696f7d38e4b20815cc
soaryong-c <soaryong@stamper.network>
2022-05-25 20:09:25 +0900

    [ANDWL-196] fix bugs - moniker crash

--- Cosmostation/Cell/AllValidatorCell.swift
+++ Cosmostation/Cell/AllValidatorCell.swift
@@ -51,7 +51,9 @@ class AllValidatorCell: UITableViewCell {
     func updateView(_ validator: Cosmos_Staking_V1beta1_Validator, _ chainType: ChainType?) {
         powerLabel.attributedText =  WUtils.displayAmount2(validator.tokens, powerLabel.font, WUtils.mainDivideDecimal(chainType), 6)
         commissionLabel.attributedText = WUtils.getDpEstAprCommission(commissionLabel.font, NSDecimalNumber.init(string: validator.commission.commissionRates.rate).multiplying(byPowerOf10: -18), chainType!)
-        validatorImg.af_setImage(withURL: URL(string: WUtils.getMonikerImgUrl(chainType, validator.operatorAddress))!)
+        if let url = URL(string: WUtils.getMonikerImgUrl(chainType, validator.operatorAddress)) {
+            validatorImg.af_setImage(withURL: url)
+        }
         
         monikerLabel.text = validator.description_p.moniker
         monikerLabel.adjustsFontSizeToFitWidth = true
