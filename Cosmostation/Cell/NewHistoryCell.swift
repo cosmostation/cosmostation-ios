e8a12f7d3bb6f7d655278538fbeab9adde01a2d9
yongjoo <yongjoo@wannabit.io>
2022-07-13 10:40:17 +0900

    [IOS]History font size adjsut

--- Cosmostation/Cell/NewHistoryCell.swift
+++ Cosmostation/Cell/NewHistoryCell.swift
@@ -21,7 +21,7 @@ class NewHistoryCell: UITableViewCell {
         super.awakeFromNib()
         self.selectionStyle = .none
         txTimeLabel.font = UIFontMetrics(forTextStyle: .caption1).scaledFont(for: Font_12_caption1)
-        txTimeGapLabel.font = UIFontMetrics(forTextStyle: .caption1).scaledFont(for: Font_11_caption2)
+        txTimeGapLabel.font = UIFontMetrics(forTextStyle: .caption1).scaledFont(for: Font_12_caption1)
         txAmountLabel.font = UIFontMetrics(forTextStyle: .caption1).scaledFont(for: Font_12_caption1)
     }
     
