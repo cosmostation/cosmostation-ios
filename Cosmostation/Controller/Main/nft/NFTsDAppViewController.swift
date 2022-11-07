7196c7af413d4f8b2e5f20606a671594e3365562
albertopeam <alberto__pa@hotmail.com>
2022-11-07 23:28:48 +0100

    removed dead breaks
    In swift cases are exclusive, not need to break to avoid jumping into
    next cases

--- Cosmostation/Controller/Main/nft/NFTsDAppViewController.swift
+++ Cosmostation/Controller/Main/nft/NFTsDAppViewController.swift
@@ -218,18 +218,12 @@ extension UIImage {
         case .down, .downMirrored:
             transform = transform.translatedBy(x: self.size.width, y: self.size.height)
             transform = transform.rotated(by: .pi)
-            break
-            
         case .left, .leftMirrored:
             transform = transform.translatedBy(x: self.size.width, y: 0)
             transform = transform.rotated(by: .pi / 2)
-            break
-            
         case .right, .rightMirrored:
             transform = transform.translatedBy(x: 0, y: self.size.height)
             transform = transform.rotated(by: -.pi / 2)
-            break
-            
         default:
             break
         }
@@ -238,13 +232,9 @@ extension UIImage {
         case .upMirrored, .downMirrored:
             transform = transform.translatedBy(x: self.size.width, y: 0)
             transform = transform.scaledBy(x: -1, y: 1)
-            break
-            
         case .leftMirrored, .rightMirrored:
             transform = transform.translatedBy(x: self.size.height, y: 0);
             transform = transform.scaledBy(x: -1, y: 1)
-            break
-            
         default:
             break
         }
@@ -255,11 +245,8 @@ extension UIImage {
         switch self.imageOrientation {
         case .left, .leftMirrored, .right, .rightMirrored:
             ctx?.draw(self.cgImage!, in: CGRect(x: CGFloat(0), y: CGFloat(0), width: CGFloat(size.height), height: CGFloat(size.width)))
-            break
-            
         default:
             ctx?.draw(self.cgImage!, in: CGRect(x: CGFloat(0), y: CGFloat(0), width: CGFloat(size.width), height: CGFloat(size.height)))
-            break
         }
         
         let cgimg: CGImage = (ctx?.makeImage())!
