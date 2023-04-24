860268c5fdd7baf59b8ff82df29ecf8d926b36be
yongjoo <yongjoo@wannabit.io>
2023-04-24 20:57:06 +0900

    [IOS]Temp add

---
+++ Cosmostation/Cell/neutron/SubVaultCell.swift
@@ -0,0 +1,24 @@
+//
+//  SubVaultCell.swift
+//  Cosmostation
+//
+//  Created by yongjoo jung on 2023/04/24.
+//  Copyright © 2023 wannabit. All rights reserved.
+//
+
+import UIKit
+
+class SubVaultCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Cosmostation/Controller/Main/neutron/VaultsListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  VaultsListViewController.swift
+//  Cosmostation
+//
+//  Created by yongjoo jung on 2023/04/24.
+//  Copyright © 2023 wannabit. All rights reserved.
+//
+
+import UIKit
+
+class VaultsListViewController: UIViewController {
+
+    @IBOutlet weak var vaultsListTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
