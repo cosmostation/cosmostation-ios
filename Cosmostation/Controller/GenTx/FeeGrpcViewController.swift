0149e2902fde1b61d64d53444c658155ef2d6baf
yongjoo <yongjoo@wannabit.io>
2023-08-20 16:13:50 +0900

    [Hotfix]Skip gas check logic only for asset-mantle

--- Cosmostation/Controller/GenTx/FeeGrpcViewController.swift
+++ Cosmostation/Controller/GenTx/FeeGrpcViewController.swift
@@ -101,6 +101,8 @@ class FeeGrpcViewController: BaseViewController, SBCardPopupDelegate {
     }
     
     @objc func onClickFeeDenom (_ onClickDenom: UITapGestureRecognizer) {
+        // 23.8.20 side case for assetmantle simulte error skip simul only for assetmantle
+        if (chainType == .MANTLE_MAIN) { return }
         let popupVC = SelectPopupViewController(nibName: "SelectPopupViewController", bundle: nil)
         popupVC.type = SELECT_POPUP_FEE_DENOM
         popupVC.feeData = mFeeInfo[mSelectedFeeInfo].FeeDatas
@@ -119,9 +121,16 @@ class FeeGrpcViewController: BaseViewController, SBCardPopupDelegate {
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
+        // 23.8.20 side case for assetmantle simulte error skip simul only for assetmantle
+        if (chainType == .MANTLE_MAIN) {
+            self.onCalculateFees()
+            self.onUpdateView()
+            self.mSimulPassed = true
+            return
+        }
         if (self.pageHolderVC.mTransferType == TRANSFER_EVM) {
             self.onCalculateEvmFees(self.chainConfig!)
-            
+
         } else {
             self.mSimulPassed = false
             self.onCalculateFees()
