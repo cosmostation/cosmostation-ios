440e450602f0968cda8036eff798ec342aac6d41
yongjoo <yongjoo@wannabit.io>
2023-09-26 23:36:49 +0900

    update stake info

--- Cosmostation/UI/Cell/StakeDelegateCell.swift
+++ Cosmostation/UI/Cell/StakeDelegateCell.swift
@@ -48,10 +48,17 @@ class StakeDelegateCell: UITableViewCell {
         if let msAsset = BaseData.instance.getAsset(baseChain.apiName, stakeDenom) {
             
             let vpAmount = NSDecimalNumber(string: validator.tokens).multiplying(byPowerOf10: -msAsset.decimals!)
-            vpLabel?.attributedText = WDP.dpAmount(vpAmount.stringValue, vpLabel!.font, msAsset.decimals!)
+            vpLabel?.attributedText = WDP.dpAmount(vpAmount.stringValue, vpLabel!.font, 0)
             
             let commission = NSDecimalNumber(string: validator.commission.commissionRates.rate).multiplying(byPowerOf10: -16)
             commLabel?.attributedText = WDP.dpAmount(commission.stringValue, commLabel!.font, 2)
+            if (commission == NSDecimalNumber.zero) {
+                commLabel.textColor = .colorGreen
+                commPercentLabel.textColor = .colorGreen
+            } else {
+                commLabel.textColor = .color02
+                commPercentLabel.textColor = .color02
+            }
             
             let stakedAmount = NSDecimalNumber(string: delegation.balance.amount).multiplying(byPowerOf10: -msAsset.decimals!)
             stakingLabel?.attributedText = WDP.dpAmount(stakedAmount.stringValue, stakingLabel!.font, msAsset.decimals!)
