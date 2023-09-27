f77c93f6ee251379560eb2d9ff222da02de8bb0f
yongjoo <yongjoo@wannabit.io>
2023-09-28 00:05:11 +0900

    add ongoing

--- Cosmostation/Controller/Main/CosmosClass/GenTx/CosmosClaimRewards.swift
+++ Cosmostation/Controller/Main/CosmosClass/GenTx/CosmosClaimRewards.swift
@@ -9,10 +9,21 @@
 import UIKit
 
 class CosmosClaimRewards: BaseVC {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var validatorsLabel: UILabel!
+    @IBOutlet weak var rewardAmountLabel: UILabel!
+    @IBOutlet weak var rewardDenomLabel: UILabel!
+    @IBOutlet weak var rewardCntLabel: UILabel!
+    @IBOutlet weak var rewardCurrencyLabel: UILabel!
+    @IBOutlet weak var rewardValueLabel: UILabel!
+    
+    
+    
+    @IBOutlet weak var claimBtn: BaseButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 }
