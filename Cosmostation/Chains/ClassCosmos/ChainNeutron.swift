fd4dcff0cf8c1d14647232d7966fa6c69f071845
yongjoo <yongjoo@wannabit.io>
2023-11-20 22:32:13 +0900

    [APP-822]Neutron api update (fetching faster)

--- Cosmostation/Chains/ClassCosmos/ChainNeutron.swift
+++ Cosmostation/Chains/ClassCosmos/ChainNeutron.swift
@@ -39,34 +39,29 @@ class ChainNeutron: CosmosClass  {
     }
     
     override func fetchPropertyData(_ channel: ClientConnection, _ id: Int64) {
-        Task {
-            if let vault = try? await self.fetchVaults(),
-               let daos = try? await self.fetchDaos() {
-                self.vaultsList = vault
-                self.daosList = daos
-            }
+        self.vaultsList = getChainParam()["vaults"].arrayValue
+        self.daosList = getChainParam()["daos"].arrayValue
+        
+        let group = DispatchGroup()
+        
+        fetchBalance(group, channel)
+        fetchNeutronVesting(group, channel)
+        if (vaultsList.count > 0) {
+            fetchVaultDeposit(group, channel)
+        }
+        
+        group.notify(queue: .main) {
+            try? channel.close()
+            WUtils.onParseVestingAccount(self)
+            self.fetched = true
+            self.allCoinValue = self.allCoinValue()
+            self.allCoinUSDValue = self.allCoinValue(true)
             
-            let group = DispatchGroup()
-            
-            fetchBalance(group, channel)
-            fetchNeutronVesting(group, channel)
-            if (vaultsList.count > 0) {
-                fetchVaultDeposit(group, channel)
-            }
-            
-            group.notify(queue: .main) {
-                try? channel.close()
-                WUtils.onParseVestingAccount(self)
-                self.fetched = true
-                self.allCoinValue = self.allCoinValue()
-                self.allCoinUSDValue = self.allCoinValue(true)
-                
-                BaseData.instance.updateRefAddressesMain(
-                    RefAddress(id, self.tag, self.bechAddress, self.evmAddress,
-                               self.allStakingDenomAmount().stringValue, self.allCoinUSDValue.stringValue,
-                               nil, self.cosmosBalances?.count))
-                NotificationCenter.default.post(name: Notification.Name("FetchData"), object: self.tag, userInfo: nil)
-            }
+            BaseData.instance.updateRefAddressesMain(
+                RefAddress(id, self.tag, self.bechAddress, self.evmAddress,
+                           self.allStakingDenomAmount().stringValue, self.allCoinUSDValue.stringValue,
+                           nil, self.cosmosBalances?.count))
+            NotificationCenter.default.post(name: Notification.Name("FetchData"), object: self.tag, userInfo: nil)
         }
     }
     
@@ -88,14 +83,6 @@ class ChainNeutron: CosmosClass  {
 }
 
 extension ChainNeutron {
-    
-    func fetchVaults() async throws -> [JSON] {
-        return try await AF.request(NEUTRON_MAIN_VAULTS, method: .get).serializingDecodable([JSON].self).value
-    }
-    
-    func fetchDaos() async throws -> [JSON] {
-        return try await AF.request(NEUTRON_MAIN_DAOS, method: .get).serializingDecodable([JSON].self).value
-    }
     
     func fetchVaultDeposit(_ group: DispatchGroup, _ channel: ClientConnection) {
         group.enter()
@@ -167,7 +154,3 @@ extension ChainNeutron {
 
 //Neutron Contract Address
 let NEUTRON_VESTING_CONTRACT_ADDRESS = "neutron1h6828as2z5av0xqtlh4w9m75wxewapk8z9l2flvzc29zeyzhx6fqgp648z"
-
-let NEUTRON_MAIN_VAULTS = "https://raw.githubusercontent.com/cosmostation/chainlist/main/chain/neutron/vaults.json"
-let NEUTRON_MAIN_DAOS = "https://raw.githubusercontent.com/cosmostation/chainlist/main/chain/neutron/daos.json"
-
