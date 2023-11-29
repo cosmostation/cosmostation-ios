b62483b00243f61468e41727fefa3a1be536850b
yongjoo <yongjoo@wannabit.io>
2023-11-29 15:03:54 +0900

    [APP-828]QA feedback v2  - matched nameservice ui update

--- Cosmostation/Controller/Sheet/SheetCell/SelectNameServiceCell.swift
+++ Cosmostation/Controller/Sheet/SheetCell/SelectNameServiceCell.swift
@@ -9,8 +9,6 @@
 import UIKit
 
 class SelectNameServiceCell: UITableViewCell {
-    
-    @IBOutlet weak var chainLogoImg: UIImageView!
     @IBOutlet weak var serviceLabel: UILabel!
     @IBOutlet weak var addressLabel: UILabel!
 
@@ -21,19 +19,15 @@ class SelectNameServiceCell: UITableViewCell {
     
     func onBindNameservice(_ nameservice: NameService) {
         if (nameservice.type == "starname") {
-            chainLogoImg.image = UIImage(named: "chainStarname")
             serviceLabel.text = "Starname Name Service"
             
         } else if (nameservice.type == "osmosis") {
-            chainLogoImg.image = UIImage(named: "chainOsmosis")
             serviceLabel.text = "Osmosis ICNS"
             
         } else if (nameservice.type == "stargaze") {
-            chainLogoImg.image = UIImage(named: "chainStargaze")
             serviceLabel.text = "Stargaze Name Service"
             
         } else if (nameservice.type == "archway") {
-            chainLogoImg.image = UIImage(named: "chainAkash")
             serviceLabel.text = "Archway ARCH ID"
         }
         addressLabel.text = nameservice.address
