a1a4536826e545e6866ba8ccbb7beab326406143
yongjoo <yongjoo@wannabit.io>
2023-12-27 22:40:11 +0900

    [APP-848]Bug fixed wrong unbonding time

--- Cosmostation/UI/Cell/AboutStakingCell.swift
+++ Cosmostation/UI/Cell/AboutStakingCell.swift
@@ -22,28 +22,36 @@ class AboutStakingCell: UITableViewCell {
         super.awakeFromNib()
         selectionStyle = .none
         rootView.setBlur()
+        unbondingTimeLabel.text = "-"
+        inflationLabel.text = "-"
+        stakingAprLabel.text = "-"
     }
     
     override func prepareForReuse() {
         rootView.setBlur()
+        unbondingTimeLabel.text = "-"
+        inflationLabel.text = "-"
+        stakingAprLabel.text = "-"
     }
     
     func onBindStakingInfo(_ chain: CosmosClass, _ json: JSON) {
         
         let unbondingSec = json["params"]["staking_params"]["params"]["unbonding_time"].stringValue.filter({ $0.isNumber })
-        let time = UInt64(unbondingSec) ?? 1814400
-        let unbondingDay = UInt16(time / 24 / 60 / 60)
-        unbondingTimeLabel.text = String(unbondingDay) + " Days"
-        
+        if let time = UInt64(unbondingSec) {
+            let unbondingDay = UInt16(time / 24 / 60 / 60)
+            unbondingTimeLabel.text = String(unbondingDay) + " Days"
+        }
         
         let nf = WUtils.getNumberFormatter(2)
-        let inflation = json["params"]["minting_inflation"]["inflation"].string ?? "0"
-        let formatInflation = nf.string(from: NSDecimalNumber(string: inflation).multiplying(byPowerOf10: 2))!
-        inflationLabel.attributedText = WUtils.getDpAttributedString(formatInflation, 2, inflationLabel.font)
+        if let inflation = json["params"]["minting_inflation"]["inflation"].string {
+            let formatInflation = nf.string(from: NSDecimalNumber(string: inflation).multiplying(byPowerOf10: 2))!
+            inflationLabel.attributedText = WUtils.getDpAttributedString(formatInflation, 2, inflationLabel.font)
+        }
         
-        let apr = json["params"]["apr"].string ?? "0"
-        let formatApr = nf.string(from: NSDecimalNumber(string: apr).multiplying(byPowerOf10: 2))!
-        stakingAprLabel.attributedText = WUtils.getDpAttributedString(formatApr, 2, stakingAprLabel.font)
+        if let apr = json["params"]["apr"].string {
+            let formatApr = nf.string(from: NSDecimalNumber(string: apr).multiplying(byPowerOf10: 2))!
+            stakingAprLabel.attributedText = WUtils.getDpAttributedString(formatApr, 2, stakingAprLabel.font)
+        }
     }
     
 }
