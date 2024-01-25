7b19ab1880334b226b4256a138249eb0cec1890f
yongjoo <yongjoo@wannabit.io>
2024-01-25 19:15:50 +0900

    [APP-833]Add vc for evm chains

---
+++ Cosmostation/Controller/Main/EvmClass/EvmHistoryVC.swift
@@ -0,0 +1,30 @@
+//
+//  EvmHistoryVC.swift
+//  Cosmostation
+//
+//  Created by yongjoo jung on 2024/01/25.
+//  Copyright © 2024 wannabit. All rights reserved.
+//
+
+import UIKit
+
+class EvmHistoryVC: BaseVC {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
