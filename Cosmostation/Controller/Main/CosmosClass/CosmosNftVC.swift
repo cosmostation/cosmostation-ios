9a9183a62c6582a9a36bcd642ce41ae3053db534
yongjoo <yongjoo@wannabit.io>
2024-03-23 18:38:38 +0900

    [APP-921]Prepare support Cw721

--- Cosmostation/Controller/Main/CosmosClass/CosmosNftVC.swift
+++ Cosmostation/Controller/Main/CosmosClass/CosmosNftVC.swift
@@ -7,24 +7,18 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
+import Lottie
 
 class CosmosNftVC: BaseVC {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var emptyDataView: UIView!
+    @IBOutlet weak var loadingView: LottieAnimationView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
