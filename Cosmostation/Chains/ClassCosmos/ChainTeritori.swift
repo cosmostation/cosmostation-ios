c6ef1147d0b1410e103578ac7abbb736dda5f113
yongjoo <yongjoo@wannabit.io>
2024-03-25 03:16:13 +0900

    [APP-921]Enable cw721 archway, juno, teri, tera

--- Cosmostation/Chains/ClassCosmos/ChainTeritori.swift
+++ Cosmostation/Chains/ClassCosmos/ChainTeritori.swift
@@ -23,6 +23,7 @@ class ChainTeritori: CosmosClass  {
         accountKeyType = AccountKeyType(.COSMOS_Secp256k1, "m/44'/118'/0'/0/X")
         bechAccountPrefix = "tori"
         validatorPrefix = "torivaloper"
+        supportCw721 = true
         
         grpcHost = "grpc-teritori.cosmostation.io"
     }
