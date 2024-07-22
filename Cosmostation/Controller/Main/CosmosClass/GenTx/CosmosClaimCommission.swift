e191cb8897e82ecad44af21759b3f1fac93b92a7
cha-somin <somin@stamper.network>
2024-07-22 17:01:28 +0900

    [APP-988] Change Feemarket naming

--- Cosmostation/Controller/Main/CosmosClass/GenTx/CosmosClaimCommission.swift
+++ Cosmostation/Controller/Main/CosmosClass/GenTx/CosmosClaimCommission.swift
@@ -124,10 +124,10 @@ class CosmosClaimCommission: BaseVC {
     func oninitFeeView() {
         if (cosmosFetcher.cosmosBaseFees.count > 0) {
             feeSegments.removeAllSegments()
-            feeSegments.insertSegment(withTitle: "No Tip", at: 0, animated: false)
-            feeSegments.insertSegment(withTitle: "20% Tip", at: 1, animated: false)
-            feeSegments.insertSegment(withTitle: "50% Tip", at: 2, animated: false)
-            feeSegments.insertSegment(withTitle: "100% Tip", at: 3, animated: false)
+            feeSegments.insertSegment(withTitle: "Default", at: 0, animated: false)
+            feeSegments.insertSegment(withTitle: "Fast", at: 1, animated: false)
+            feeSegments.insertSegment(withTitle: "Faster", at: 2, animated: false)
+            feeSegments.insertSegment(withTitle: "Instant", at: 3, animated: false)
             feeSegments.selectedSegmentIndex = selectedFeePosition
             
             let baseFee = cosmosFetcher.cosmosBaseFees[0]
