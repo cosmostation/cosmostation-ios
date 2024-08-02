ec570fa0a5b35fdcc7a24934bbefdc56b7fe5bf0
yongjoo <yongjoo@wannabit.io>
2024-08-02 18:41:47 +0900

    [APP-1021]Portfoilo for sui

--- Cosmostation/Chains/ChainSui.swift
+++ Cosmostation/Chains/ChainSui.swift
@@ -20,12 +20,10 @@ class ChainSui: BaseChain  {
         logo1 = "chainSui"
         apiName = "sui"
         accountKeyType = AccountKeyType(.SUI_Ed25519, "m/44'/784'/0'/0'/X'")
-        
-        
+    
         coinSymbol = "SUI"
         stakeDenom = SUI_MAIN_DENOM
         coinGeckoId = "sui"
-        
         
         mainUrl = "https://sui-mainnet-us-2.cosmostation.io"
     }
@@ -34,23 +32,19 @@ class ChainSui: BaseChain  {
         privateKey = priKey
         publicKey = KeyFac.getPubKeyFromPrivateKey(privateKey!, accountKeyType.pubkeyType)
         mainAddress = KeyFac.getAddressFromPubKey(publicKey!, accountKeyType.pubkeyType, nil)
-        
-        mainAddress = "0x00878369f475a454939af7b84cdd981515b1329f159a1aeb9bf0f8899e00083a"
     }
     
-    func getSuifetcher() -> SuiFetcher? {
+    func getSuiFetcher() -> SuiFetcher? {
         if (suiFetcher != nil) { return suiFetcher }
         suiFetcher = SuiFetcher(self)
         return suiFetcher
     }
     
     override func fetchBalances() {
-    }
-    
-    override func fetchData(_ id: Int64) {
         fetchState = .Busy
         Task {
-            let suiResult = await getSuifetcher()?.fetchSuiData(id)
+            coinsCnt = 0
+            let suiResult = await getSuiFetcher()?.fetchSuiBalances()
             
             if (suiResult == false) {
                 fetchState = .Fail
@@ -58,14 +52,48 @@ class ChainSui: BaseChain  {
                 fetchState = .Success
             }
             
-            if let suiFetcher = getSuifetcher(), fetchState == .Success {
+            if (self.fetchState == .Success) {
+                if let suiFetcher = getSuiFetcher() {
+                    coinsCnt = suiFetcher.suiBalances.count
+                }
+            }
+            
+            DispatchQueue.main.async(execute: {
+                NotificationCenter.default.post(name: Notification.Name("fetchBalances"), object: self.tag, userInfo: nil)
+            })
+        }
+    }
+    
+    override func fetchData(_ id: Int64) {
+        fetchState = .Busy
+        Task {
+            let suiResult = await getSuiFetcher()?.fetchSuiData(id)
+            
+            if (suiResult == false) {
+                fetchState = .Fail
+            } else {
+                fetchState = .Success
+            }
+            
+            if let suiFetcher = getSuiFetcher(), fetchState == .Success {
                 print("suiBalances ", suiFetcher.suiBalances.count)
                 print("suiStakedList ", suiFetcher.suiStakedList.count)
                 print("suiObjects ", suiFetcher.suiObjects.count)
+                print("suiBalances ", suiFetcher.suiBalances.count)
                 
+                coinsCnt = suiFetcher.suiBalances.count
                 
+                allCoinValue = suiFetcher.allValue()
+                allCoinUSDValue = suiFetcher.allValue(true)
+                let mainCoinAmount = suiFetcher.allSuiAmount()
                 
+                allTokenValue = NSDecimalNumber.zero
+                allTokenUSDValue = NSDecimalNumber.zero
                 
+                BaseData.instance.updateRefAddressesValue(
+                    RefAddress(id, self.tag, self.mainAddress, "",
+                               mainCoinAmount.stringValue, allCoinUSDValue.stringValue, allTokenUSDValue.stringValue,
+                               coinsCnt))
             }
             
             DispatchQueue.main.async(execute: {
--- Cosmostation/Fetcher/SuiFetcher.swift
+++ Cosmostation/Fetcher/SuiFetcher.swift
@@ -25,9 +25,28 @@ class SuiFetcher {
         self.chain = chain
     }
     
+    func fetchSuiBalances() async -> Bool {
+        suiBalances.removeAll()
+        if let balance = try? await fetchAllBalances(chain.mainAddress) {
+            balance?["result"].arrayValue.forEach({ balance in
+                let type = balance["coinType"].stringValue
+                let amount = NSDecimalNumber.init(string: balance["totalBalance"].stringValue)
+                if (type == SUI_TYPE_COIN) {
+                    suiBalances.append((SUI_MAIN_DENOM, amount))
+                } else {
+                    suiBalances.append((type, amount))
+                }
+            })
+            suiBalances.sort {
+                if ($0.0 == SUI_MAIN_DENOM) { return true }
+                if ($1.0 == SUI_MAIN_DENOM) { return false }
+                return false
+            }
+        }
+        return true
+    }
     
     func fetchSuiData(_ id: Int64) async -> Bool {
-        
         suiSystem = JSON()
         suiBalances.removeAll()
         suiStakedList.removeAll()
@@ -38,7 +57,7 @@ class SuiFetcher {
         do {
             if let chainidentifier = try await fetchChainId(),
                let latestSuiSystemState = try await fetchSystemState(),
-               let ownedObjects = try? await fetchOwnedObjects(chain.mainAddress),
+               let _ = try? await fetchOwnedObjects(chain.mainAddress, nil),
                let stakes = try? await fetchStakes(chain.mainAddress) {
                 
                 suiSystem = latestSuiSystemState["result"]
@@ -51,10 +70,7 @@ class SuiFetcher {
                     return $0["votingPower"].intValue > $1["votingPower"].intValue ? true : false
                 }
                 
-                //TODO check page
-                ownedObjects?["result"]["data"].arrayValue.forEach({ data in
-                    suiObjects.append(data["data"])
-                })
+                
                 suiObjects.forEach { object in
                     let type = object["type"].stringValue
                     if (type.starts(with: SUI_TYPE_COIN)) {
@@ -92,6 +108,112 @@ class SuiFetcher {
         }
     }
     
+    func stakedAmount() -> NSDecimalNumber {
+        var staked = NSDecimalNumber.zero
+        var earned = NSDecimalNumber.zero
+        suiStakedList.forEach { suiStaked in
+            suiStaked["stakes"].arrayValue.forEach { stakes in
+                staked = staked.adding(NSDecimalNumber(value: stakes["principal"].int64Value))
+                earned = earned.adding(NSDecimalNumber(value: stakes["estimatedReward"].int64Value))
+            }
+        }
+        return staked.adding(earned)
+    }
+    
+    func stakedValue(_ usd: Bool? = false) -> NSDecimalNumber {
+        let amount = stakedAmount()
+        if (amount == NSDecimalNumber.zero) { return NSDecimalNumber.zero }
+        if let msAsset = BaseData.instance.getAsset(chain.apiName, SUI_MAIN_DENOM) {
+            let msPrice = BaseData.instance.getPrice(msAsset.coinGeckoId, usd)
+            return msPrice.multiplying(by: amount).multiplying(byPowerOf10: -msAsset.decimals!, withBehavior: handler6)
+        }
+        return NSDecimalNumber.zero
+    }
+    
+    func principalAmount() -> NSDecimalNumber {
+        var staked = NSDecimalNumber.zero
+        suiStakedList.forEach { suiStaked in
+            suiStaked["stakes"].arrayValue.forEach { stakes in
+                staked = staked.adding(NSDecimalNumber(value: stakes["principal"].int64Value))
+            }
+        }
+        return staked
+    }
+    
+    func principalValue(_ usd: Bool? = false) -> NSDecimalNumber {
+        let amount = principalAmount()
+        if (amount == NSDecimalNumber.zero) { return NSDecimalNumber.zero }
+        if let msAsset = BaseData.instance.getAsset(chain.apiName, SUI_MAIN_DENOM) {
+            let msPrice = BaseData.instance.getPrice(msAsset.coinGeckoId, usd)
+            return msPrice.multiplying(by: amount).multiplying(byPowerOf10: -msAsset.decimals!, withBehavior: handler6)
+        }
+        return NSDecimalNumber.zero
+    }
+    
+    func estimatedRewardAmount() -> NSDecimalNumber {
+        var earned = NSDecimalNumber.zero
+        suiStakedList.forEach { suiStaked in
+            suiStaked["stakes"].arrayValue.forEach { stakes in
+                earned = earned.adding(NSDecimalNumber(value: stakes["estimatedReward"].int64Value))
+            }
+        }
+        return earned
+    }
+    
+    func estimatedRewardValue(_ usd: Bool? = false) -> NSDecimalNumber {
+        let amount = estimatedRewardAmount()
+        if (amount == NSDecimalNumber.zero) { return NSDecimalNumber.zero }
+        if let msAsset = BaseData.instance.getAsset(chain.apiName, SUI_MAIN_DENOM) {
+            let msPrice = BaseData.instance.getPrice(msAsset.coinGeckoId, usd)
+            return msPrice.multiplying(by: amount).multiplying(byPowerOf10: -msAsset.decimals!, withBehavior: handler6)
+        }
+        return NSDecimalNumber.zero
+    }
+    
+    
+    func balanceAmount(_ type: String) -> NSDecimalNumber {
+        if let suiCoin = suiBalances.filter({ $0.0 == type }).first {
+            return suiCoin.1
+        }
+        return NSDecimalNumber.zero
+    }
+    
+    func balanceValue(_ type: String, _ usd: Bool? = false) -> NSDecimalNumber {
+        let amount = balanceAmount(type)
+        if (amount == NSDecimalNumber.zero) { return NSDecimalNumber.zero }
+        if let msAsset = BaseData.instance.getAsset(chain.apiName, type) {
+            let msPrice = BaseData.instance.getPrice(msAsset.coinGeckoId, usd)
+            return msPrice.multiplying(by: amount).multiplying(byPowerOf10: -msAsset.decimals!, withBehavior: handler6)
+        }
+        return NSDecimalNumber.zero
+    }
+    
+    func allBalanceValue(_ usd: Bool? = false) -> NSDecimalNumber {
+        var result =  NSDecimalNumber.zero
+        suiBalances.forEach { balance in
+            result = result.adding(balanceValue(balance.0, usd))
+        }
+        return result
+    }
+    
+    func allSuiAmount() -> NSDecimalNumber {
+        return stakedAmount().adding(balanceAmount(SUI_MAIN_DENOM))
+    }
+    
+    func allSuiValue(_ usd: Bool? = false) -> NSDecimalNumber {
+        let amount = allSuiAmount()
+        if (amount == NSDecimalNumber.zero) { return NSDecimalNumber.zero }
+        if let msAsset = BaseData.instance.getAsset(chain.apiName, SUI_MAIN_DENOM) {
+            let msPrice = BaseData.instance.getPrice(msAsset.coinGeckoId, usd)
+            return msPrice.multiplying(by: amount).multiplying(byPowerOf10: -msAsset.decimals!, withBehavior: handler6)
+        }
+        return NSDecimalNumber.zero
+    }
+    
+    func allValue(_ usd: Bool? = false) -> NSDecimalNumber {
+        return allBalanceValue(usd).adding(stakedValue(usd))
+    }
+    
     
     func getSuiRpc() -> String {
         return chain.mainUrl
@@ -99,6 +221,12 @@ class SuiFetcher {
 }
 
 
+/**
+ *   suix_getAllBalances         gives sui coin type as  0x2::coin::Coin
+ *   suix_getOwnedObjects    gives sui coin type as  0x2::coin::Coin<0x2::sui::SUI>
+ *
+ *    we using 0x2::coin::Coin<0x2::sui::SUI> as amin sui default coin denom
+ */
 extension SuiFetcher {
     
     func fetchChainId() async throws -> JSON? {
@@ -111,15 +239,28 @@ extension SuiFetcher {
         return try await AF.request(getSuiRpc(), method: .post, parameters: parameters, encoding: JSONEncoding.default).serializingDecodable(JSON.self).value
     }
     
-    func fetchOwnedObjects(_ address: String) async throws -> JSON? {
-        let params: Any = [address, ["filter": nil, "options":["showContent":true, "showDisplay":true,  "showType":true]]]
-        let parameters: Parameters = ["method": "suix_getOwnedObjects", "params": params, "id" : 1, "jsonrpc" : "2.0"]
-//        return try await AF.request(getSuiRpc(), method: .post, parameters: parameters, encoding: JSONEncoding.default).serializingDecodable(JSON.self).value
-        
-        if let result = try? await AF.request(getSuiRpc(), method: .post, parameters: parameters, encoding: JSONEncoding.default).serializingDecodable(JSON.self).value {
-            
+    func fetchAllBalances(_ address: String) async throws -> JSON?  {
+        let parameters: Parameters = ["method": "suix_getAllBalances", "params": [address], "id" : 1, "jsonrpc" : "2.0"]
+        return try await AF.request(getSuiRpc(), method: .post, parameters: parameters, encoding: JSONEncoding.default).serializingDecodable(JSON.self).value
+    }
+    
+    func fetchOwnedObjects(_ address: String, _ cursor: String?) async throws {
+        var params: Any!
+        if (cursor == nil) {
+            params = [address, ["filter": nil, "options":["showContent":true, "showDisplay":true,  "showType":true]]]
+        } else {
+            params = [address, ["filter": nil, "options":["showContent":true, "showDisplay":true,  "showType":true]], cursor!]
         }
-        return nil
+        let parameters: Parameters = ["method": "suix_getOwnedObjects", "params": params, "id" : 1, "jsonrpc" : "2.0"]
+        if let response = try? await AF.request(getSuiRpc(), method: .post, parameters: parameters, encoding: JSONEncoding.default).serializingDecodable(JSON.self).value {
+//            print("response ", response)
+            response["result"]["data"].arrayValue.forEach({ data in
+                suiObjects.append(data["data"])
+            })
+            if (response["result"]["hasNextPage"].bool == true && response["result"]["nextCursor"].string != nil) {
+                try await fetchOwnedObjects(address, response["result"]["nextCursor"].stringValue)
+            }
+        }
     }
     
     func fetchStakes(_ address: String) async throws -> JSON? {
