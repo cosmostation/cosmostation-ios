051d1e29fa30806672fbd5b7b6e63ecdefb9a402
yongjoo <yongjoo@wannabit.io>
2024-11-05 15:21:27 +0900

    [APP-1074]Update Evm chains logo

--- Cosmostation/Chains/ClassCosmos/ChainFetchAi.swift
+++ Cosmostation/Chains/ClassCosmos/ChainFetchAi.swift
@@ -15,7 +15,7 @@ class ChainFetchAi: BaseChain  {
         
         name = "ASI Alliance"
         tag = "fetchai118"
-        logo1 = "chainASIAllianceEVM"
+        logo1 = "chainASIAlliance"
         apiName = "fetchai"
         accountKeyType = AccountKeyType(.COSMOS_Secp256k1, "m/44'/118'/0'/0/X")
         
