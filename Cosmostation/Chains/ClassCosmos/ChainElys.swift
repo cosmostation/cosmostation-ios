0db26f650d8f6943fa0273ffb98c88ba36e36a4b
cha-somin <somin@stamper.network>
2025-01-08 18:08:22 +0900

    Update ChainElys endpoint

--- Cosmostation/Chains/ClassCosmos/ChainElys.swift
+++ Cosmostation/Chains/ClassCosmos/ChainElys.swift
@@ -20,12 +20,12 @@ class ChainElys: BaseChain  {
         accountKeyType = AccountKeyType(.COSMOS_Secp256k1, "m/44'/118'/0'/0/X")
         
         
-        cosmosEndPointType = .UseGRPC
+        cosmosEndPointType = .UseLCD
         stakeDenom = "uelys"
         bechAccountPrefix = "elys"
         validatorPrefix = "elysvaloper"
         supportStaking = false
-        grpcHost = "grpc-elys.cosmostation.io"
-        lcdUrl = "lcd-elys.cosmostation.io/"
+        grpcHost = ""
+        lcdUrl = "https://elys-api.polkachu.com/"
     }
 }
