8940fb71d628328a3a068e13654051ed41b71f49
cha-somin <somin@stamper.network>
2025-01-20 18:36:58 +0900

    Update ChainDungeon.swift

--- Cosmostation/Chains/ClassCosmos/ChainDungeon.swift
+++ Cosmostation/Chains/ClassCosmos/ChainDungeon.swift
@@ -24,7 +24,7 @@ class ChainDungeon: BaseChain  {
         stakeDenom = "udgn"
         bechAccountPrefix = "dungeon"
         validatorPrefix = "dungeonvaloper"
-        grpcHost = "dungeon.grpc.quasarstaking.ai"
+        grpcHost = ""
         lcdUrl = "https://dungeon-wallet.api.quasarstaking.ai"
     }
     
